a5eba2cfb925200e5f5506d90618aef6e53c9934
Deepa Ramam <dshenoy@gmail.com>
2017-07-17 15:23:22 -0700

    initial commit

---
+++ models/User.js
@@ -0,0 +1,22 @@
+var Sequelize = require('sequelize');
+module.exports = function(app) {
+  if (app === undefined) {
+    app = this;
+  }
+  var sequelize = app.get('sequelize');
+  var User = sequelize.define('User', {
+    id: {
+      type: "STRING"
+    },
+    name: {
+      type: "STRING"
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        models.User.hasMany(models.Todo);
+      }
+    }
+  });
+  return User;
+};
